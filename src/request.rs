09e8fecd226e922686c4bdc215cec33efee2debd
Amanda Graven <amanda@amandag.net>
2021-08-20 13:50:30 +0200

    feat: Add conversion from http Request types

--- src/request.rs
+++ src/request.rs
@@ -262,19 +262,66 @@ impl Request {
 	}
 
 	/// Convert a reqwest request into a requeuest request.
-	pub fn from_reqwest(foreign: reqwest::Request) -> Self {
+	pub fn from_reqwest(mut foreign: reqwest::Request) -> Self {
 		Self {
 			url: foreign.url().to_owned(),
 			body: foreign.body().and_then(|b| b.as_bytes()).map(|b| b.to_vec()),
-			method: foreign.method().to_owned(),
-			headers: foreign.headers().to_owned(),
+			method: std::mem::take(foreign.method_mut()),
+			headers: std::mem::take(foreign.headers_mut()),
 			accept_responses: default_accepted_responses(),
 		}
+	}
+
+	/// Constructs a request by converting a request builder from the `http`
+	/// crate. Returns `None` if the uri or the method are missing from the
+	/// builder
+	#[cfg(feature = "http")]
+	pub fn from_http_builder(
+		foreign: http::request::Builder,
+		body: Option<Vec<u8>>,
+	) -> Result<Self, crate::error::ConvertError> {
+		match body {
+			Some(body) => Ok(Self::from_http_body(foreign.body(body)?)?),
+			None => Ok(Self::from_http_empty(foreign.body(())?)?),
+		}
+	}
+
+	#[cfg(feature = "http")]
+	fn from_http_parts(parts: http::request::Parts) -> Result<Self, url::ParseError> {
+		Ok(Self {
+			url: Url::parse(&parts.uri.to_string())?,
+			body: None,
+			method: parts.method,
+			headers: parts.headers,
+			accept_responses: default_accepted_responses(),
+		})
+	}
+
+	/// Convert a [`http::Request`] with a body into a requeuest request.
+	#[cfg(feature = "http")]
+	pub fn from_http_body<B: Into<Vec<u8>>>(
+		foreign: http::Request<B>,
+	) -> Result<Self, url::ParseError> {
+		let (parts, body) = foreign.into_parts();
+		let mut request = Self::from_http_parts(parts)?;
+		request.body = Some(body.into());
+		Ok(request)
+	}
+
+	/// Convert a [`http::Request`] without a body into a requeuest request
+	#[cfg(feature = "http")]
+	pub fn from_http_empty<B>(foreign: http::Request<B>) -> Result<Self, url::ParseError> {
+		let (parts, _) = foreign.into_parts();
+		Self::from_http_parts(parts)
 	}
 }
 
 #[cfg(test)]
 mod tests {
+	use reqwest::{
+		header::{HeaderValue, AUTHORIZATION},
+		Method,
+	};
 	use url::Url;
 
 	use super::Request;
@@ -291,5 +338,67 @@ mod tests {
 		assert_eq!(request.method, deserialized.method);
 		assert_eq!(request.body, deserialized.body);
 		assert_eq!(request.headers, deserialized.headers);
+	}
+
+	#[test]
+	fn convert_reqwest() {
+		let mut foreign = reqwest::Request::new(Method::POST, "https://foo.bar/".parse().unwrap());
+		foreign.headers_mut().insert(AUTHORIZATION, HeaderValue::from_static("Secret"));
+		*foreign.body_mut() = Some("body".into());
+		let request = Request::from_reqwest(foreign);
+
+		assert_eq!(request.url.to_string(), "https://foo.bar/", "URL mismatch");
+		assert_eq!(request.method, Method::POST, "Method mismatch");
+		assert_eq!(request.headers.get(AUTHORIZATION).unwrap(), &"Secret", "Header mismatch");
+		assert_eq!(request.body.unwrap(), b"body", "Body mismatch");
+	}
+
+	#[cfg(feature = "http")]
+	#[test]
+	fn convert_http_builder() {
+		let foreign = http::Request::post("https://foo.bar/").header(AUTHORIZATION, "Secret");
+		let request = Request::from_http_builder(foreign, Some(b"body".to_vec())).unwrap();
+
+		assert_eq!(request.url.to_string(), "https://foo.bar/", "URL mismatch");
+		assert_eq!(request.method, Method::POST, "Method mismatch");
+		assert_eq!(request.headers.get(AUTHORIZATION).unwrap(), &"Secret", "Header mismatch");
+		assert_eq!(request.body.unwrap(), b"body", "Body mismatch");
+
+		let bad = http::request::Builder::new();
+		assert!(Request::from_http_builder(bad, None).is_none(), "Missing value guard failed");
+	}
+
+	#[cfg(feature = "http")]
+	#[test]
+	fn convert_http_empty() {
+		let foreign = http::Request::put("https://bar.baz/")
+			.header(AUTHORIZATION, "Credentials")
+			.body(())
+			.unwrap();
+		let request = Request::from_http_empty(foreign).unwrap();
+
+		assert_eq!(request.url.to_string(), "https://bar.baz/", "URL mismatch");
+		assert_eq!(request.method, Method::PUT, "Method mismatch");
+		assert_eq!(request.headers.get(AUTHORIZATION).unwrap(), &"Credentials", "Header mismatch");
+		assert_eq!(request.body, None, "Body unexpectedly not empty");
+	}
+
+	#[cfg(feature = "http")]
+	#[test]
+	fn convert_http_body() {
+		let foreign = http::Request::delete("http://web.site/thing")
+			.header(AUTHORIZATION, "Bearer: l0tsofl3tters")
+			.body("yeet the thing")
+			.unwrap();
+		let request = Request::from_http_body(foreign).unwrap();
+
+		assert_eq!(request.url.to_string(), "http://web.site/thing", "URL mismatch");
+		assert_eq!(request.method, Method::DELETE, "Method mismatch");
+		assert_eq!(
+			request.headers.get(AUTHORIZATION).unwrap(),
+			&"Bearer: l0tsofl3tters",
+			"Header mismatch"
+		);
+		assert_eq!(request.body.unwrap(), b"yeet the thing", "Body mismatch");
 	}
 }
