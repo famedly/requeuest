56c29ebc9e393014487230049cd22fe8974b0bf5
Amanda Graven <amanda@amandag.net>
2021-09-02 09:34:45 +0200

    test: Add integration tests
    Add integration tests that use a hyper server to verify that the library
    behaves as expected, and that request data is not unintentially modified
    or left out.

---
+++ tests/send.rs
@@ -0,0 +1,174 @@
+//! Tests that verify that HTTP requeuests are correctly sent
+
+use std::{
+	iter::FromIterator,
+	sync::atomic::{AtomicBool, AtomicU32, Ordering},
+	time::Duration,
+};
+
+use requeuest::{
+	self,
+	client::{Channels, Client},
+	request::Request,
+	HeaderMap,
+};
+use reqwest::header::{HeaderValue, AUTHORIZATION};
+use tokio::sync::Notify;
+
+static INSTALL_EYRE: std::sync::Once = std::sync::Once::new();
+
+fn install_eyre() {
+	INSTALL_EYRE.call_once(|| color_eyre::install().expect("Installing eyre failed"))
+}
+
+/// Crate a hyper `Service` with the given future as its `service_fn`
+#[macro_export]
+macro_rules! service {
+	($closure:expr) => {
+		hyper::service::make_service_fn(|_conn| async {
+			Ok::<_, hyper::Error>(hyper::service::service_fn($closure))
+		})
+	};
+}
+
+/// Create a hyper `Server` with the given graceful shutdown closure, returning
+/// the ip address and server object.
+#[macro_export]
+macro_rules! server {
+	($service:expr, $shutdown:expr) => {{
+		let addr = std::net::SocketAddr::from(([127, 0, 0, 1], 0));
+		let server = hyper::Server::bind(&addr).serve($service);
+		// Get the address from the server so we know what port was picked
+		let addr = server.local_addr();
+		let server = server.with_graceful_shutdown($shutdown);
+		(addr, server)
+	}};
+}
+
+static SEND_EMPTY_NOTIF: Notify = Notify::const_new();
+
+/// Verifies that a request without a body is correctly sent
+#[sqlx_database_tester::test(pool(variable = "pool", skip_migrations))]
+#[ntest::timeout(60_000)]
+async fn send_empty() -> color_eyre::eyre::Result<()> {
+	install_eyre();
+	requeuest::migrate(&pool).await?;
+	let client = Client::new(pool, Channels::All).await?;
+
+	let service = service!(|req| async move {
+		assert_eq!(req.method(), hyper::Method::GET, "Wrong method");
+		assert_eq!(req.uri().path(), "/path", "Wrong URI path");
+		assert_eq!(req.uri().query().unwrap(), "query=foo&param=bar", "Wrong URI query");
+		assert_eq!(req.headers()[AUTHORIZATION], &"Bearer: secret", "Wrong HTTP header");
+		SEND_EMPTY_NOTIF.notify_one();
+		Ok::<_, hyper::Error>(hyper::Response::new(hyper::Body::from("OK")))
+	});
+
+	let (addr, server) = server!(service, async { SEND_EMPTY_NOTIF.notified().await });
+
+	let headers =
+		HeaderMap::from_iter([(AUTHORIZATION, HeaderValue::from_static("Bearer: secret"))]);
+	let request =
+		Request::get(format!("http://{}/path?query=foo&param=bar", addr).parse()?, headers);
+
+	client.spawn("channel", &request).await?;
+	println!("sent request");
+
+	server.await?;
+
+	Ok(())
+}
+
+static RETRY_COUNT: AtomicU32 = AtomicU32::new(0);
+static RETRY_NOTIF: Notify = Notify::const_new();
+
+/// Verifies that a request is correctly retried
+#[sqlx_database_tester::test(pool(variable = "pool", skip_migrations))]
+#[ntest::timeout(60_000)]
+async fn retry() -> color_eyre::eyre::Result<()> {
+	install_eyre();
+	requeuest::migrate(&pool).await?;
+	let client = Client::new(pool, Channels::All).await?;
+
+	let service = service!(|_req| async move {
+		let attempt = RETRY_COUNT.fetch_add(1, Ordering::SeqCst);
+		let response = match attempt {
+			0..=2 => {
+				hyper::Response::builder().status(400).body(hyper::Body::from("Try again")).unwrap()
+			}
+			3 => {
+				RETRY_NOTIF.notify_one();
+				hyper::Response::new(hyper::Body::from("OK"))
+			}
+			_ => panic!("Too many retries!"),
+		};
+		Ok::<_, hyper::Error>(response)
+	});
+
+	let (addr, server) = server!(service, async { RETRY_NOTIF.notified().await });
+
+	let request = Request::get(format!("http://{}/", addr).parse()?, Default::default());
+
+	client
+		.spawn_cfg("channel", &request, |req| {
+			req.set_retries(3);
+			req.set_retry_backoff(Duration::from_millis(10));
+		})
+		.await?;
+
+	server.await?;
+
+	Ok(())
+}
+
+static TRANSACTION_NOTIF: Notify = Notify::const_new();
+static RECEIVED_ONE: AtomicBool = AtomicBool::new(false);
+static RECEIVED_TWO: AtomicBool = AtomicBool::new(false);
+static RECEIVED_THREE: AtomicBool = AtomicBool::new(false);
+/// Verifies that multiple tests sent via a transaction are correctly sent (or
+/// not sent)
+#[sqlx_database_tester::test(pool(variable = "pool", skip_migrations))]
+#[ntest::timeout(60_000)]
+async fn transaction() -> color_eyre::eyre::Result<()> {
+	install_eyre();
+	requeuest::migrate(&pool).await?;
+	let client = Client::new(pool, Channels::All).await?;
+
+	let service = service!(|req: hyper::Request<hyper::Body>| async move {
+		let (_parts, body) = req.into_parts();
+		match hyper::body::to_bytes(body).await?.as_ref() {
+			b"One" => RECEIVED_ONE.store(true, Ordering::SeqCst),
+			b"Two" => RECEIVED_TWO.store(true, Ordering::SeqCst),
+			b"Three" => RECEIVED_THREE.store(true, Ordering::SeqCst),
+			_ => panic!("Unexpected body"),
+		};
+		if RECEIVED_ONE.load(Ordering::SeqCst)
+			&& RECEIVED_TWO.load(Ordering::SeqCst)
+			&& RECEIVED_THREE.load(Ordering::SeqCst)
+		{
+			TRANSACTION_NOTIF.notify_one();
+		}
+		Ok::<_, hyper::Error>(hyper::Response::new(hyper::Body::from("OK")))
+	});
+
+	let (addr, server) = server!(service, async { TRANSACTION_NOTIF.notified().await });
+
+	let mut transaction = client.pool().begin().await?;
+
+	let request1 =
+		Request::post(format!("http://{}/", addr).parse()?, b"One".to_vec(), Default::default());
+	let request2 =
+		Request::post(format!("http://{}/", addr).parse()?, b"Two".to_vec(), Default::default());
+	let request3 =
+		Request::post(format!("http://{}/", addr).parse()?, b"Three".to_vec(), Default::default());
+
+	client.spawn_with(&mut transaction, "channel", &request1).await?;
+	client.spawn_with(&mut transaction, "channel", &request2).await?;
+	client.spawn_with(&mut transaction, "channel", &request3).await?;
+
+	transaction.commit().await?;
+
+	server.await?;
+
+	Ok(())
+}
